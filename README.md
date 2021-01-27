474ae7b24d4e6f8588f5277f8792f8baa084a034
DESKTOP-9HG5C4S\11440 <1144027420@qq.com>
2021-01-27 10:30:50 +0800

    add context

---
+++ README.md
@@ -0,0 +1,5 @@
+main 启动入口
+gee 封装http请求 ，实现http.Handler 
+router l路由分装
+context 请求上下文
+
---
+++ src/base1/trie.go
@@ -0,0 +1,28 @@
+package base1
+
+type node struct {
+	pattren  string
+	part     string
+	children []*node
+	isWild   bool
+}
+
+// 第一个匹配成功的节点，用于插入
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
